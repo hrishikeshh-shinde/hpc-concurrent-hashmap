fc8282e94d6784482bb6fc933df7fab7569f7748
Chirag Jain <chiragjain@Chirags-MacBook-Pro.local>
2025-04-09 02:51:53 -0500

    modified rehash test to test the bucket size before and after inserting 6th element

--- tests/ChainHashMapTest.cpp
+++ tests/ChainHashMapTest.cpp
@@ -35,23 +35,11 @@ void testRehash() {
 
   assert(h1.size() == 5);
 
+  assert(h1.getBuckets() == 100);
   assert(h1.insert("test1"));
-  assert(h1.insert("abc2"));
-  assert(h1.insert("pqrs3"));
-  assert(h1.insert("wxyz4"));
-  
   assert(h1.getBuckets() == 200);
-  assert(h1.size() == 9);
 
-  assert(h1.search("test"));
-  assert(h1.search("abc"));
-  assert(h1.search("pqrs"));
-  assert(h1.search("wxyz"));
-  assert(h1.search("wxyz"));
-  assert(h1.search("test1"));
-  assert(h1.search("abc2"));
-  assert(h1.search("pqrs3"));
-  assert(h1.search("wxyz4"));
+  assert(h1.size() == 6);
 
   std::cout << "testRehash() PASSED" << "\n";
 }
